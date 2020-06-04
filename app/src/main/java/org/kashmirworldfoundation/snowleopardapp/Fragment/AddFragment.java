5d783ab197606a32b55ea87b53570f23804232a9
Gogster7 <gogsterog@gmail.com>
2020-06-04 16:19:45 -0400

    Attempt to Merge code

--- app/src/main/java/org/kashmirworldfoundation/snowleopardapp/Fragment/AddFragment.java
+++ app/src/main/java/org/kashmirworldfoundation/snowleopardapp/Fragment/AddFragment.java
@@ -1,24 +1,916 @@
 package org.kashmirworldfoundation.snowleopardapp.Fragment;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import org.kashmirworldfoundation.snowleopardapp.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import java.util.Calendar;
+import java.util.Date;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 public class AddFragment extends Fragment {
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public android.view.View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_add, container, false);
     }
+
+    private static final String TAG = "Activity";
+
+
+    private EditText stationIdInput;
+
+
+    private EditText WatershedInput;
+
+
+    private EditText LatitudeInput;
+
+
+    private EditText LongitudeInput;
+
+
+    private EditText ElevationInput;
+
+
+    private EditText cameraIDInput;
+
+
+    private double latitude;
+
+
+    private double longitude;
+
+
+    private String stationId;
+
+
+    private String watershedid;
+
+
+    private String latitudeS;
+
+
+    private String longitudeS;
+
+
+    private String altitude;
+
+
+    private String cameraId;
+
+
+
+
+
+    private String terrain;
+
+
+    private String habitat;
+
+
+    private String lureType;
+
+
+    private String substrate;
+
+
+    private String potential;
+
+
+
+
+
+    private static final int LOCATION_REQUEST = 111;
+
+
+    //private boolean isEmpty;
+
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
+
+
+
+
+    @Override
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.fragment_add);
+
+
+
+
+
+        stationIdInput=findViewById(R.id.stationId);
+
+
+        WatershedInput=findViewById(R.id.watershedid);
+
+
+        LatitudeInput=findViewById(R.id.nId);
+
+
+        LongitudeInput=findViewById(R.id.eId);
+
+
+        ElevationInput=findViewById(R.id.elevationId);
+
+
+        cameraIDInput=findViewById(R.id.cameraId);
+
+
+
+
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        checkPermission();
+
+
+        determineLocation();
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+    @Override
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        getMenuInflater().inflate(R.menu.create_station_menu, menu);
+
+
+        return true;
+
+
+    }
+
+
+
+
+
+    public void getInput(){
+
+
+        Log.d(TAG, "In getInput ");
+
+
+        stationId=stationIdInput.getText().toString();
+
+
+        watershedid=WatershedInput.getText().toString();
+
+
+        latitudeS=LatitudeInput.getText().toString();
+
+
+        longitudeS=LatitudeInput.getText().toString();
+
+
+        altitude=ElevationInput.getText().toString();
+
+
+        cameraId=cameraIDInput.getText().toString();
+
+
+    }
+
+
+
+
+
+    //lure type
+
+
+    public void radioClicked_SkunkFishOil(View v) {
+
+
+        lureType = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "LureType You Selected " + lureType, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_CastorFishOil(View v) {
+
+
+        lureType = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "LureType You Selected " + lureType, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_FishOil(View v) {
+
+
+        lureType = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "LureType You Selected " + lureType, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_None(View v) {
+
+
+        lureType = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "LureType You Selected " + lureType, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    //habitat type
+
+
+    public void radioClicked_Scrub(View v) {
+
+
+        habitat = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Habitat You Selected " + habitat, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_Forest(View v) {
+
+
+        habitat = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Habitat You Selected " + habitat, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_Pasture(View v) {
+
+
+        habitat = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Habitat You Selected " + habitat, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_Barren(View v) {
+
+
+        habitat = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Habitat You Selected " + habitat, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_Agric(View v) {
+
+
+        habitat = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Habitat You Selected " + habitat, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    //terrain type
+
+
+    public void radioClicked_Ridge(View v) {
+
+
+        terrain = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + terrain, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_Cliff(View v) {
+
+
+        terrain = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + terrain, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_Draw(View v) {
+
+
+        terrain = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + terrain, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_Vally(View v) {
+
+
+        terrain = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + terrain, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_Saddle(View v) {
+
+
+        terrain = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + terrain, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_plateau(View v) {
+
+
+        terrain = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + terrain, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    //substrate type
+
+
+    public void radioClicked_sand(View v) {
+
+
+        substrate = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + substrate, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_soil(View v) {
+
+
+        substrate = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + substrate, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_rock(View v) {
+
+
+        substrate = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + substrate, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_snow(View v) {
+
+
+        substrate = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + substrate, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_vegetation(View v) {
+
+
+        substrate = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + substrate, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    //potential
+
+
+    public void radioClicked_Good(View v) {
+
+
+        potential = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + potential, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_Medium(View v) {
+
+
+        potential = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + potential, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    public void radioClicked_Poor(View v) {
+
+
+        potential = ((RadioButton) v).getText().toString();
+
+
+        Toast.makeText(this, "Terrain You Selected " + potential, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    @Override
+
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+
+
+
+
+        Log.d(TAG, "onOptionsItemSelected: "+item.getItemId());
+
+
+        switch (item.getItemId()) {
+
+
+            case android.R.id.home:
+
+
+                //Toast.makeText(this,"back",Toast.LENGTH_SHORT).show();
+
+
+                finish();
+
+
+                return super.onOptionsItemSelected(item);
+
+
+
+
+
+            case R.id.saveId:
+
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+
+                builder.setIcon(R.drawable.kwflogo);
+
+
+
+
+
+                final Date currentTime = Calendar.getInstance().getTime();
+
+
+
+
+
+
+
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+
+                    public void onClick(DialogInterface dialog, int id) {
+
+
+                        //Toast.makeText(EditProfileActivity.this, "OK", Toast.LENGTH_SHORT).show();
+
+
+                        createToast(AddFragment.this,"Create Station Successful :"+currentTime.toString(), Toast.LENGTH_LONG);
+
+
+                        getInput();
+
+
+                    }
+
+
+                });
+
+
+
+
+
+                builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+
+
+                    public void onClick(DialogInterface dialog, int id) {
+
+
+                        Toast.makeText(AddFragment.this, "CANCEL", Toast.LENGTH_SHORT).show();
+
+
+                    }
+
+
+                });
+
+
+
+
+
+                builder.setTitle("Save the Station?");
+
+
+                AlertDialog dialog = builder.create();
+
+
+                dialog.show();
+
+
+
+
+
+                break;
+
+
+            default:
+
+
+                Toast.makeText(this, "Pick the unknown item", Toast.LENGTH_LONG).show();
+
+
+        }
+
+
+
+
+
+        return true;
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+    private void determineLocation() {
+
+
+        Log.d(TAG, "determineLocation: !!!!"+checkPermission());
+
+
+        if (checkPermission()) {
+
+
+            Log.d(TAG, "determineLocation: 22222");
+
+
+            mFusedLocationClient.getLastLocation()
+
+
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+
+
+                        @Override
+
+
+                        public void onSuccess(Location location) {
+
+
+                            if (location != null) {
+
+
+                                latitude=location.getLatitude();
+
+
+                                longitude=location.getLongitude();
+
+
+                                LatitudeInput.setText(String.valueOf(latitude));
+
+
+                                Log.d(TAG, "determineLocation: 333"+latitude);
+
+
+                                LongitudeInput.setText(String.valueOf(longitude));
+
+
+                                Log.d(TAG, "determineLocation: 44444"+longitude);
+
+
+                            }
+
+
+                        }
+
+
+                    });
+
+
+        }
+
+
+    }
+
+
+
+
+
+    private boolean checkPermission() {
+
+
+        if (ContextCompat.checkSelfPermission(this,
+
+
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+
+
+                PackageManager.PERMISSION_GRANTED) {
+
+
+
+
+
+            ActivityCompat.requestPermissions(this,
+
+
+                    new String[]{
+
+
+                            Manifest.permission.ACCESS_FINE_LOCATION
+
+
+                    }, LOCATION_REQUEST);
+
+
+            return false;
+
+
+        }
+
+
+        return true;
+
+
+    }
+
+
+
+
+
+    public static void createToast(Context context, String message, int time) {
+
+
+        Toast toast = Toast.makeText(context, "" + message, time);
+
+
+        View toastView = toast.getView();
+
+
+        toastView.setBackgroundColor(Color.parseColor("#008577"));
+
+
+        TextView tv = toast.getView().findViewById(android.R.id.message);
+
+
+        tv.setPadding(140, 75, 140, 75);
+
+
+        tv.setTextColor(0xFFFFFFFF);
+
+
+        toast.show();
+
+
+    }
+
+
+
+
+
+    @Override
+
+
+    public void onRequestPermissionsResult(int requestCode,
+
+
+                                           @NonNull String[] permissions,
+
+
+                                           @NonNull int[] grantResults) {
+
+
+
+
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+
+
+
+        if (requestCode == LOCATION_REQUEST) {
+
+
+            for (int i = 0; i < permissions.length; i++) {
+
+
+                if (permissions[i].equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+
+                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+
+
+                        determineLocation();
+
+
+                    } else {
+
+
+                        Toast.makeText(this, "Location Permission not Granted", Toast.LENGTH_SHORT).show();
+
+
+                    }
+
+
+                }
+
+
+            }
+
+
+        }
+
+
+    }
+
 
 }
--- app/src/main/java/org/kashmirworldfoundation/snowleopardapp/MainActivity.java
+++ app/src/main/java/org/kashmirworldfoundation/snowleopardapp/MainActivity.java
@@ -1,9 +1,14 @@
 package org.kashmirworldfoundation.snowleopardapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.ListFragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,66 +16,99 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.kashmirworldfoundation.snowleopardapp.Fragment.AddFragment;
 import org.kashmirworldfoundation.snowleopardapp.Fragment.ProfileFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
-    Fragment selectedFragment = null;
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    private ListFragment listFragment;
+    private AddFragment addFragment;
+    private ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
+       toolbar = findViewById(R.id.toolbar);
+       setSupportActionBar(toolbar);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+       viewPager = findViewById(R.id.view_pager);
+       tabLayout = findViewById(R.id.tab_layout);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                new ListFragment()).commit();
+       listFragment = new ListFragment();
+       addFragment = new AddFragment();
+       profileFragment = new ProfileFragment();
 
+       tabLayout.setupWithViewPager(viewPager);
+
+       // Setting up Tabs
+
+       ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), 0);
+       viewPagerAdapter.addFragment(listFragment, "List");
+       viewPagerAdapter.addFragment(addFragment, "Add");
+       viewPagerAdapter.addFragment(profileFragment, "Profile");
+       viewPager.setAdapter(viewPagerAdapter);
+       // Icons
+       tabLayout.getTabAt(0).setIcon(R.drawable.ic_list);
+       tabLayout.getTabAt(1).setIcon(R.drawable.ic_add);
+       tabLayout.getTabAt(2).setIcon(R.drawable.ic_profile);
+       // Badges
+        //BadgeDrawable badgeDrawable = tabLayout.getTabAt(0).getOrCreateBadge();
+        //badgeDrawable.setVisible(true);
+        //badgeDrawable.setNumber(10);
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-                    switch (menuItem.getItemId()){
-                        case R.id.nav_list:
-                            selectedFragment = new ListFragment();
-                            break;
-
-                        case R.id.nav_add:
-                            selectedFragment = new AddFragment();
-                            //startActivity(new Intent(MainActivity.this, PostActivity.class));
-                            break;
-
-                        case R.id.nav_profile:
-//                            SharedPreferences.Editor editor = getSharedPreferences( "PREFS", MODE_PRIVATE).edit();
-//                            editor.putString("profilied", FirebaseAuth.getInstance().getCurrentUser().getUid());
-//                            editor.apply();
-                            selectedFragment = new ProfileFragment();
-                            break;
-                    }
-
-                    if (selectedFragment != null){
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                                selectedFragment).commit();
-                    }
-
-                    return true;
-                }
-            };
 
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(), Login.class));
         finish();
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragments = new ArrayList<>();
+        private List<String> fragmentTitle = new ArrayList<>();
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragments.add(fragment);
+            fragmentTitle.add(title);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitle.get(position);
+        }
     }
 }
