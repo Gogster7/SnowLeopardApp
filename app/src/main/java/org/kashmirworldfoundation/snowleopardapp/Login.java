439138fd27b7a9fc5a847ed8f7353be83b41e571
MatthewPham1675 <56077811+MatthewPham1675@users.noreply.github.com>
2020-08-21 11:06:46 -0400

    Update Login.java
    load background from firestore

--- app/src/main/java/org/kashmirworldfoundation/snowleopardapp/Login.java
+++ app/src/main/java/org/kashmirworldfoundation/snowleopardapp/Login.java
@@ -1,116 +1,140 @@
-package org.kashmirworldfoundation.snowleopardapp;
+<?xml version="1.0" encoding="utf-8"?>
+<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:id="@+id/LoginLayout"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:background="@drawable/background"
+    tools:context=".Login">
+    <ImageView
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+        android:id="@+id/BackgroundLogin"
+        android:layout_width="411dp"
+        android:layout_height="735dp"
+        tools:layout_editor_absoluteX="0dp"
+        tools:layout_editor_absoluteY="-5dp"
+        tools:srcCompat="@tools:sample/backgrounds/scenic"
+        android:visibility="invisible"
+        android:scaleType="fitXY"/>
+    <TextView
+        android:id="@+id/RegisterOrgBtn"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="New Organization? Register Here"
+        android:textColor="#FAF5F7"
+        android:textSize="18sp"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent"
+        app:layout_constraintVertical_bias="0.84000003" />
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
+    <TextView
+        android:id="@+id/Register0"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="New Member? Register Here"
+        android:textColor="#FAF5F7"
+        android:textSize="18sp"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent"
+        app:layout_constraintVertical_bias="0.758" />
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+    <Button
+        android:id="@+id/LoginBtn"
+        android:layout_width="362dp"
+        android:layout_height="55dp"
+        android:background="#FFFF8D"
+        android:fontFamily="@font/aclonica"
+        android:text="LOGIN"
+        android:textColor="#1B1A1A"
+        android:textSize="24sp"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintHorizontal_bias="0.673"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toBottomOf="@+id/password"
+        app:layout_constraintVertical_bias="0.214" />
 
-public class Login extends AppCompatActivity {
-    EditText mEmail, mPassword;
-    Button mLoginBtn;
-    TextView mRegisterBtn,mRegisterOrgBtn;
-    FirebaseAuth fAuth;
-    ProgressBar progressBar;
-    FirebaseFirestore fStore;
+    <EditText
+        android:id="@+id/password"
+        android:layout_width="375dp"
+        android:layout_height="42dp"
+        android:backgroundTint="#FAFAFA"
+        android:ems="10"
+        android:fontFamily="@font/aclonica"
+        android:hint="Password:"
+        android:inputType="textPassword"
+        android:textColorHint="#F8F8F8"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent" />
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        fAuth= FirebaseAuth.getInstance();
-        fStore= FirebaseFirestore.getInstance();
-        /*
-        if(fAuth.getCurrentUser() != null) {
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            finish();
-        }
+    <EditText
+        android:id="@+id/email"
+        android:layout_width="375dp"
+        android:layout_height="42dp"
+        android:backgroundTint="#FAFAFA"
+        android:ems="10"
+        android:fontFamily="@font/aclonica"
+        android:hint="Email:"
+        android:inputType="textPersonName"
+        android:textColorHint="#F8F8F8"
+        app:layout_constraintBottom_toTopOf="@+id/password"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toBottomOf="@+id/textView3" />
+
+    <TextView
+        android:id="@+id/textView3"
+        android:layout_width="66dp"
+        android:layout_height="32dp"
+        android:elegantTextHeight="true"
+        android:fontFamily="@font/aclonica"
+        android:shadowColor="#B4B1B1"
+        android:text="Login"
+        android:textColor="#FFFF8D"
+        android:textSize="18sp"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent"
+        app:layout_constraintVertical_bias="0.26999998" />
+
+    <TextView
+        android:id="@+id/textView"
+        android:layout_width="288dp"
+        android:layout_height="45dp"
+        android:elegantTextHeight="true"
+        android:fontFamily="@font/aclonica"
+        android:shadowColor="#B4B1B1"
+        android:text="Snow Leopard"
+        android:textColor="#FFFFFF"
+        android:textSize="36sp"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintHorizontal_bias="0.495"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent"
+        app:layout_constraintVertical_bias="0.19999999" />
+
+    <ProgressBar
+        android:id="@+id/progressBar"
+        style="?android:attr/progressBarStyle"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:visibility="invisible"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintHorizontal_bias="0.498"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent"
+        app:layout_constraintVertical_bias="0.92" />
 
 
-         */
 
-        mEmail      = findViewById(R.id.email);
-        mPassword   = findViewById(R.id.password);
-        mLoginBtn   = findViewById(R.id.LoginBtn);
-        mRegisterBtn = findViewById(R.id.Register0);
-        mRegisterOrgBtn = findViewById(R.id.RegisterOrgBtn);
-        fAuth       = FirebaseAuth.getInstance();
-        progressBar = findViewById(R.id.progressBar);
-
-
-        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), RegisterSpinner0.class));
-               
-            }
-        });
-        mRegisterOrgBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), RegisterOrg.class));
-            }
-        });
-        mLoginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email= mEmail.getText().toString().trim();
-                String pass =mPassword.getText().toString().trim();
-                fAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-
-
-                            fStore.document("Member/"+fAuth.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                    if (task.isSuccessful()){
-
-                                        saveMember(task.getResult().toObject(Member.class),fAuth.getUid());
-                                        saveCamNum();
-                                        Toast.makeText(Login.this,"Welcome", Toast.LENGTH_LONG).show();
-                                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                                    }
-                                }
-                            });
-                        }
-                        else{
-                            Toast.makeText(Login.this, "Error" + task.getException().getMessage(), Toast.LENGTH_LONG ).show();
-                        }
-
-                    }
-                });
-            }
-        });
-
-    }
-    private void saveMember (Member mem,String uid){
-        SharedPreferences sharedPreferences = Login.this.getSharedPreferences("user", Context.MODE_PRIVATE);
-
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        Gson gson = new Gson();
-        String json =gson.toJson(mem);
-        editor.putString("user",json);
-        editor.putString("uid",uid);
-        editor.apply();
-    }
-    private void saveCamNum(){
-        SharedPreferences sharedPreferences = Login.this.getSharedPreferences("camstations",Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putInt("CamNum",0);
-        editor.apply();
-    }
-}
-
+</androidx.constraintlayout.widget.ConstraintLayout>
