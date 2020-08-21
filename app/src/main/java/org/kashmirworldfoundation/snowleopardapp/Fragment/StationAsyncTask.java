6da2ad64a249a2649893ad1a68e41064493c3edd
MatthewPham1675 <56077811+MatthewPham1675@users.noreply.github.com>
2020-08-21 10:06:04 -0400

    Update StationAsyncTask.java

--- app/src/main/java/org/kashmirworldfoundation/snowleopardapp/Fragment/StationAsyncTask.java
+++ app/src/main/java/org/kashmirworldfoundation/snowleopardapp/Fragment/StationAsyncTask.java
@@ -1,22 +1,33 @@
 package org.kashmirworldfoundation.snowleopardapp.Fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.common.reflect.TypeToken;
 import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.gson.Gson;
 
+import org.kashmirworldfoundation.snowleopardapp.CameraStation;
 import org.kashmirworldfoundation.snowleopardapp.Fragment.ListFragment;
-import org.kashmirworldfoundation.snowleopardapp.Station;
+import org.kashmirworldfoundation.snowleopardapp.Member;
+import org.kashmirworldfoundation.snowleopardapp.Org;
 
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -25,9 +36,11 @@ public class StationAsyncTask extends AsyncTask<String, Void, String> {
 
     private FirebaseFirestore firebaseFirestore;
     private CollectionReference collectionReference;
-    private ArrayList<Station> stations=new ArrayList<>();
+    private FirebaseAuth FireAuth;
 
-
+    private ArrayList<CameraStation> CStations= new ArrayList<>();
+    private Member mem;
+    private String Org;
     private static final String TAG = "StationAsyncTask";
     private int count;
     private int size;
@@ -37,7 +50,7 @@ public class StationAsyncTask extends AsyncTask<String, Void, String> {
     StationAsyncTask(ListFragment li){listFragment=li;}
 
     protected void update(){
-        listFragment.updateStationList(stations);
+        listFragment.updateStationList(CStations);
         listFragment.updateList();
     }
 
@@ -47,6 +60,7 @@ public class StationAsyncTask extends AsyncTask<String, Void, String> {
 
         // Add data from Firebase on the the Arrays
         try {
+            FireAuth = FirebaseAuth.getInstance();
             firebaseFirestore = FirebaseFirestore.getInstance();
             collectionReference = firebaseFirestore.collection("CameraStation");
         }
@@ -54,78 +68,33 @@ public class StationAsyncTask extends AsyncTask<String, Void, String> {
             //Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
         }
 
-        try{
-            collectionReference.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                @Override
-                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                    size=queryDocumentSnapshots.size();
-                    for (DocumentSnapshot objectDocumentSnapshot:queryDocumentSnapshots)
-                    {
-                        Station newStation=new Station();
 
-                        newStation.setStationId(objectDocumentSnapshot.getString("StationId"));
-                        newStation.setAltitude(objectDocumentSnapshot.getString("altitude"));
-                        newStation.setAuthor(objectDocumentSnapshot.getString("author"));
-                        newStation.setCameraId(objectDocumentSnapshot.getString("cameraId"));
-                        newStation.setCountry(objectDocumentSnapshot.getString("country"));
-                        newStation.setHabitat(objectDocumentSnapshot.getString("habitat"));
-//                        newStation.setIDate(objectDocumentSnapshot.getString("IDate"));
-
-                        newStation.setLattitudeS(objectDocumentSnapshot.getString("lattitudeS"));
-                        newStation.setLongitudeS(objectDocumentSnapshot.getString("longitudeS"));
-
-                        newStation.setLureType(objectDocumentSnapshot.getString("lureType"));
-                        newStation.setOrg(objectDocumentSnapshot.getString("org"));
-                        newStation.setPic(objectDocumentSnapshot.getString("pic"));
-                        newStation.setAuthor(objectDocumentSnapshot.getString("author"));
-                        Timestamp s =objectDocumentSnapshot.getTimestamp("posted");
-
-
-                        if(s!=null){
-                            newStation.setPosted(s.toDate().toString());
-                        }
-
-                        newStation.setPotential(objectDocumentSnapshot.getString("potential"));
-
-                        newStation.setRegion(objectDocumentSnapshot.getString("region"));
-
-                        newStation.setSubstrate(objectDocumentSnapshot.getString("substrate"));
-                        newStation.setTerrain(objectDocumentSnapshot.getString("terrain"));
-                        newStation.setWatershedid(objectDocumentSnapshot.getString("watershedid"));
-
-                        //this part is for getting data from firebase's different name
-                        //station id
-                        String stationId = objectDocumentSnapshot.getString("StationId");
-                        if(stationId==null){
-                            stationId=objectDocumentSnapshot.getString("stationId");
-                        }
-                        newStation.setStationId(stationId);
-
-                        //date
-                        String date = objectDocumentSnapshot.getString("SDate");
-                        if(date==null){
-                            date = objectDocumentSnapshot.getTimestamp("posted").toDate().toString();
-                        }
-                        newStation.setPosted(date);
-                        stations.add(newStation);
-
-                        count++;
-                        if(count==size){
-                            update();
-                        }
-                    }
-                }
-            })
-                    .addOnFailureListener(new OnFailureListener() {
+        firebaseFirestore.collection("Member").document(FireAuth.getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    mem=task.getResult().toObject(Member.class);
+                    collectionReference.whereEqualTo("org",mem.getOrg()).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                         @Override
-                        public void onFailure(@NonNull Exception e) {
-                            //  Toast.makeText(getActivity(),"Fails to retrieve Camera Station", Toast.LENGTH_LONG).show();
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()){
+                                size = task.getResult().size();
+                                for (DocumentSnapshot objectDocumentSnapshot: task.getResult()){
+                                    CameraStation stat = objectDocumentSnapshot.toObject(CameraStation.class);
+                                    CStations.add(stat);
+                                    count++;
+                                    if(count==size){
+                                        update();
+                                    }
+                                }
+                            }
                         }
                     });
-        }
-        catch (Exception e){
-            //Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
+                }
+            }
+        });
+
+
 
 
         return null;
