04cd2e8e5729019e743287e34333ee10e9243cdf
Gogster7 <gogsterog@gmail.com>
2020-09-15 09:23:43 -0400

    Rebait to firebase Layouts

---
+++ app/src/main/java/org/kashmirworldfoundation/snowleopardapp/Rebait.java
@@ -0,0 +1,39 @@
+package org.kashmirworldfoundation.snowleopardapp;
+
+import com.google.firebase.database.Exclude;
+
+class Rebait {
+    private String stationId;
+    private String totalPics;
+    private String signsInput;
+    private String sdNum;
+    private String lureType;
+    private String camWorks;
+    //private Date currentTime;
+
+    public Rebait() {
+
+    }
+    public Rebait(String totalPics, String signsInput, String sdNum, String lureType, String camWorks){
+        this.totalPics = totalPics;
+        this.signsInput = signsInput;
+        this.sdNum = sdNum;
+        this.lureType = lureType;
+        this.camWorks = camWorks;
+    }
+
+    @Exclude
+    public String getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(String stationId) {
+        this.stationId = stationId;
+    }
+
+    public String getTotalPics(){ return totalPics;}
+    public String getSignsInput(){ return signsInput;}
+    public String getSdNum(){ return sdNum;}
+    public String getLureType() { return lureType;}
+    public String getCamWorks() { return camWorks; }
+}
