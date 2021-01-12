b177c97801d22d936a778d79df8a2d96963c9b2a
MatthewPham1675 <56077811+MatthewPham1675@users.noreply.github.com>
2021-01-12 01:45:43 -0500

    Update RegisterOrgAdmin.java

--- app/src/main/java/org/kashmirworldfoundation/snowleopardapp/RegisterOrgAdmin.java
+++ app/src/main/java/org/kashmirworldfoundation/snowleopardapp/RegisterOrgAdmin.java
@@ -1,10 +1,13 @@
 package org.kashmirworldfoundation.snowleopardapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,59 +43,40 @@ public class RegisterOrgAdmin extends AppCompatActivity {
         mbRegisterA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                St2 = FirebaseStorage.getInstance();
-                db2  = FirebaseFirestore.getInstance();
-                final String Aname = mName.getText().toString().trim();
-                final String Aemail = mEmail.getText().toString().trim();
-                final String Apassword = mPassword.getText().toString().trim();
-                final String Ajob = mJob.getText().toString().trim();
-                final String Aphone = mPhone.getText().toString().trim();
+                Utils util = new Utils();
 
-                Intent intent = getIntent();
-                final String Orgname =intent.getStringExtra("Orgname");
-                final String Region = intent.getStringExtra("Region");
-                final String Country = intent.getStringExtra("Country");
-                //Orgname= capitalizeFirstLetter(Orgname);
-                //Region = capitalizeFirstLetter(Region);
-                //Country = capitalizeFirstLetter(Country);
-                final Member memA =new Member();
-                fAuth2.createUserWithEmailAndPassword(Aemail,Apassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            db2.collection("Organization").whereEqualTo("orgName",Orgname).whereEqualTo("orgCountry",Country).
-                                    whereEqualTo("orgRegion",Region).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                    if (task.isSuccessful()){
-                                        for (QueryDocumentSnapshot documentSnapshot: task.getResult()){
-                                            Toast.makeText(RegisterOrgAdmin.this,"Org Found", Toast.LENGTH_SHORT).show();
-                                            memA.setOrg(documentSnapshot.getReference().getPath());
 
-                                        }
-                                        memA.setJob(Ajob);
-                                        memA.setFullname(Aname);
-                                        memA.setPhone(Aphone);
-                                        memA.setAdmin(Boolean.TRUE);
-                                        memA.setEmail(Aemail);
-                                        memA.setProfile("profile/kwflogo.jpg");
-                                        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
-                                        db2.collection("Member").document(user.getUid()).set(memA).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                if(task.isSuccessful()){
-                                                    Toast.makeText(RegisterOrgAdmin.this,"User Created", Toast.LENGTH_SHORT).show();
-                                                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                                                }
-                                            }
-                                        });
-                                    }
+                if (util.getAgreement(RegisterOrgAdmin.this)){
+                    LayoutInflater inflater= LayoutInflater.from(RegisterOrgAdmin.this);
+                    View view=inflater.inflate(R.layout.disclaimer_layout, null);
 
-                                }
-                            });
+
+                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(RegisterOrgAdmin.this);
+                    alertDialog.setTitle("Terms of Service");
+                    alertDialog.setView(view);
+                    alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(RegisterOrgAdmin.this,"Agreement needed to register",Toast.LENGTH_LONG).show();
                         }
-                    }
-                });
+
+                    });
+
+                    alertDialog.setPositiveButton("Agree", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            util.setAgreement(RegisterOrgAdmin.this);
+                            registerAdmin();
+                        }
+                    });
+                    AlertDialog alert = alertDialog.create();
+                    alert.show();
+                }
+                else {
+                    registerAdmin();
+                }
+
 
             }
         });
@@ -100,5 +84,59 @@ public class RegisterOrgAdmin extends AppCompatActivity {
     public String capitalizeFirstLetter(String string) {
         string =string.toLowerCase();
         return string.substring(0,1).toUpperCase() + string.substring(1);
+    }
+    private void registerAdmin(){
+        final String Aname = mName.getText().toString().trim();
+        final String Aemail = mEmail.getText().toString().trim();
+        final String Apassword = mPassword.getText().toString().trim();
+        final String Ajob = mJob.getText().toString().trim();
+        final String Aphone = mPhone.getText().toString().trim();
+        St2 = FirebaseStorage.getInstance();
+        db2  = FirebaseFirestore.getInstance();
+        Intent intent = getIntent();
+        final String Orgname =intent.getStringExtra("Orgname");
+        final String Region = intent.getStringExtra("Region");
+        final String Country = intent.getStringExtra("Country");
+        //Orgname= capitalizeFirstLetter(Orgname);
+        //Region = capitalizeFirstLetter(Region);
+        //Country = capitalizeFirstLetter(Country);
+        final Member memA =new Member();
+        fAuth2.createUserWithEmailAndPassword(Aemail,Apassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    db2.collection("Organization").whereEqualTo("orgName",Orgname).whereEqualTo("orgCountry",Country).
+                            whereEqualTo("orgRegion",Region).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()){
+                                for (QueryDocumentSnapshot documentSnapshot: task.getResult()){
+                                    Toast.makeText(RegisterOrgAdmin.this,"Org Found", Toast.LENGTH_SHORT).show();
+                                    memA.setOrg(documentSnapshot.getReference().getPath());
+
+                                }
+                                memA.setJob(Ajob);
+                                memA.setFullname(Aname);
+                                memA.setPhone(Aphone);
+                                memA.setAdmin(Boolean.TRUE);
+                                memA.setEmail(Aemail);
+                                memA.setProfile("profile/kwflogo.jpg");
+                                FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+                                db2.collection("Member").document(user.getUid()).set(memA).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful()){
+                                            Toast.makeText(RegisterOrgAdmin.this,"User Created", Toast.LENGTH_SHORT).show();
+                                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                        }
+                                    }
+                                });
+                            }
+
+                        }
+                    });
+                }
+            }
+        });
     }
 }
