19308191706309fc423097310ca9be39be923aed
MatthewPham1675 <56077811+MatthewPham1675@users.noreply.github.com>
2021-03-10 02:02:32 -0500

    Update Utils.java

--- app/src/main/java/org/kashmirworldfoundation/snowleopardapp/Utils.java
+++ app/src/main/java/org/kashmirworldfoundation/snowleopardapp/Utils.java
@@ -16,6 +16,13 @@ public class Utils {
         Type type =new TypeToken<Member>(){}.getType();
         return gson.fromJson(json,type);
     }
+    public String loadUid(Context context){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("user", Context.MODE_PRIVATE);
+
+        return sharedPreferences.getString("uid", null);
+
+
+    }
     public boolean getAgreement(Context context){
         SharedPreferences sharedPreferences = context.getSharedPreferences("TOS", Context.MODE_PRIVATE);
         return sharedPreferences.getBoolean("tos", true);
@@ -26,5 +33,39 @@ public class Utils {
         SharedPreferences.Editor editor= sharedPreferences.edit();
         editor.putBoolean("tos",false);
         editor.apply();
+    }
+    private String loaduid(Context context){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("user",Context.MODE_PRIVATE);
+        return sharedPreferences.getString("uid",null);
+    }
+    public void saveprey(Prey prey,Context context){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("user",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        ArrayList<Prey> list=getprey(context);
+        list.add(prey);
+        Gson gson = new GsonBuilder().registerTypeAdapter(Timestamp.class,new MyDateTypeAdapter()).create();;
+        String json =gson.toJson(list);
+
+        editor.putString("prey",json);
+        editor.apply();
+
+    }
+    public ArrayList<Prey> getprey(Context context){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("user",Context.MODE_PRIVATE);
+        Gson gson = new GsonBuilder().registerTypeAdapter(Timestamp.class,new MyDateTypeAdapter()).create();;
+        String json =sharedPreferences.getString("prey",null);
+        if (json==null){
+            return new ArrayList<>();
+        }
+        else{
+
+
+            Type type = new TypeToken<ArrayList<Prey>>() {}.getType();
+            return gson.fromJson(json,type);
+
+
+
+        }
+
     }
 }
